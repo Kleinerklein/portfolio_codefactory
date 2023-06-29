4f9670e19040fbcea3451423540a59b0362d79c3
Kleinerklein <91050412+Kleinerklein@users.noreply.github.com>
2023-06-29 09:51:09 +0200

    techballs look good

--- src/app/colorobject/colorobject.component.ts
+++ src/app/colorobject/colorobject.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ITech } from 'src/data/ITech';
+import { techs } from 'src/data/techs';
+import * as THREE from 'three';
+import { DecalGeometry } from 'three-stdlib';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-colorobject',
   templateUrl: './colorobject.component.html',
   styleUrls: ['./colorobject.component.scss']
 })
-export class ColorobjectComponent {
+export class ColorobjectComponent implements  AfterViewInit {
 
+  techs: ITech [] = techs
+  ngAfterViewInit(): void {
+    for(let i = 0; i < this.techs.length; i ++){
+        this.showModel(i);
+    }
+    
+  }
+
+  showModel(index: number) {
+    //texture
+    const texture = new THREE.TextureLoader().load(this.techs[index].icon);
+    
+    // scene
+    const scene = new THREE.Scene();
+    // geometry
+    const geometry = new THREE.SphereGeometry(1, 16, 7);
+    const material = new THREE.MeshStandardMaterial({ color: '#ffffff'});
+    material.flatShading = true;
+    material.polygonOffset = true;
+    material.polygonOffsetFactor = -5;
+    // mesh
+    const mesh = new THREE.Mesh(geometry, material,);
+    scene.add(mesh);
+    // add decal ontop of geometry
+    const decal = new DecalGeometry(
+      mesh,
+      new THREE.Vector3(0,0,1.35),
+      new THREE.Euler(0,0, 0, 'YXZ'),
+      new THREE.Vector3(1.6,1.6,1.6)
+    );
+    // nwe material for decal
+    const materialBack = new THREE.MeshStandardMaterial({ 
+      color: '#ffffff',
+      map: texture,
+      transparent: true,
+      opacity: 1,
+      flatShading: true,
+      polygonOffset:true,
+      polygonOffsetFactor: -5,  
+    });
+    // add new decal
+    const meshBack = new THREE.Mesh( decal, materialBack );
+    scene.add( meshBack );
+
+    // sizes
+    const sizes = {
+      width: 150,
+      height: 150
+    };
+    // camera
+    const camera = new THREE.PerspectiveCamera(17, sizes.width / sizes.height, 0.1, 1000);
+    camera.position.z = 10;
+    scene.add(camera);
+    // light
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    light.position.set(0, 10, 10);
+    camera.add(light);
+
+    // renderer
+    // create new component for each ball to later make the positionening easier
+    const canvas = document.createElement('canvas');
+    const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+    renderer.setSize(sizes.width, sizes.height);
+
+    // controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.enablePan = false;
+    controls.enableZoom = false;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = -3;
+
+    // update renderer and controls
+    const animate = () => {
+      controls.update(); // Update the controls on each frame
+
+     
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    };
+
+    // animate and add to div 
+    animate();
+    const componentDiv = document.querySelector('.balls') as HTMLElement;
+    componentDiv.appendChild(canvas);
+    
+  }
 }
--- src/data/techs.ts
+++ src/data/techs.ts
@@ -9,4 +9,27 @@ export const techs: ITech[] =[
         name: "css",
         icon: "assets/images/tech/css.png"
     },
+    {
+        name: "javascript",
+        icon: "assets/images/tech/javascript.png"
+    },
+    {
+        name: "typescript",
+        icon: "assets/images/tech/typescript.png"
+    },
+    {
+        name: "git",
+        icon: "assets/images/tech/git.png"
+    },
+    {
+        name: "nodeJS",
+        icon: "assets/images/tech/nodejs.png"
+    },
+    {
+        name: "tailwind",
+        icon: "assets/images/tech/tailwind.png"
+    },
+    
+
+    
 ]
